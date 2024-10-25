cedc67f2d516f34c0298e11b537ea960c2464bf5
Chris Yang <yy4382@outlook.com>
2024-10-25 13:12:49 -0700

    feat: update ribbon icon and add command trigger

--- src/main.ts
+++ src/main.ts
@@ -13,19 +13,14 @@ export default class ImageUploadPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		this.addRibbonIcon("dice", "Upload images for active file", () =>
+		this.addRibbonIcon("image-up", "Upload images for active file", () =>
 			this.process(),
 		);
-
-		// This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: "sample-editor-command",
-		// 	name: "Sample editor command",
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection("Sample Editor Command");
-		// 	},
-		// });
+		this.addCommand({
+			id: "upload-image-for-active-file",
+			name: "Upload images for active file",
+			callback: () => this.process(),
+		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SettingsTab(this.app, this));
